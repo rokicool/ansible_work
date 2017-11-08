8ac71aa6a72c2e321667bf91aed8de4e28315771
Tech Acc for Ansible <ansible@opswat.com>
2017-11-07 18:34:31 -0800

    Just added UFW rule and User_Parameter linux.security.updates

---
+++ roles/dj-wasabi.zabbix-agent/lookup_plugins/ip.py
@@ -0,0 +1,19 @@
+import ansible.utils as utils
+import ansible.errors as errors
+from ansible.plugins.lookup import LookupBase
+import socket
+
+class LookupModule(LookupBase):
+
+    def __init__(self, basedir=None, **kwargs):
+        self.basedir = basedir
+
+    def run(self, terms, variables=None, **kwargs):
+
+        hostname = terms[0]
+
+        if not isinstance(hostname, basestring):
+            raise errors.AnsibleError("ip lookup expects a string (hostname)")
+
+        return [socket.gethostbyname(hostname)]
+
