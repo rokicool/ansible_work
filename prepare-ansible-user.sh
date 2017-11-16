e2915db2682a458be61c104ac8aa72c5b748d334
Tech Acc for Ansible <ansible@opswat.com>
2017-11-15 18:29:28 -0800

    added some diagnostic messages

--- prepare-ansible-user.sh
+++ prepare-ansible-user.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-# v0.1 by rokicool
+# v0.3 by rokicool
 #
 # This script is supposed to prepare machine to be managed from anstible server.
 # It will:
@@ -15,6 +15,8 @@ ANSIBLE_UID=1499
 AUTH_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC56DZ56CMl4e8aYIfhRY7+IHa8BbXqUpq7BTQpdozRVQBGQ7QkmTVO6/b/8AjGkK2XzIJUQrT8kboNiPkTFdSkPREwJwUglIyVbw8Hy+jJG7oz+uOCYGa4ntZtxkQ7yydpTnWCPE0qtgD8NaP7C2idJ31gaiJ5668G5xiXMeKh4VAV772CIDidfZs8vQPh2TioaFxPNrLD+H1SXkyjOxhYBdGnKccaT9IZgwEh75+G8p+keeHEN0jBeSJByumppDGMdRNGAMAAVhO8osQhTnXVuKT9QMKWWx/vHzGceaBwl6XmCiyNEdKm9wYLfVNsDZCzO07XwKLdILBKLz+orAJ/ "
 
 AUTH_USER=ansible@sf-ansible-01
+
+NOISE=true
 
 print_help() {
    echo ""
@@ -84,7 +86,7 @@ create_user() {
 
 
 
-# make suer we are runnunt as root 
+# make suer we are runnung as root 
 
 if [ "$EUID" -ne 0 ]; then
    print_help
@@ -97,40 +99,66 @@ fi
 # check if the group ${ANSIBLE_GROUP} exist
 if ! check_if_group_exist ${ANSIBLE_GROUP} ; then
    # group  ${ANSIBLE_GROUP} does NOT exist - creating
+   [ ${NOISE} ] || echo "${0}: Creating ${ANSIBLE_GROUP} group with GID (${ANSIBLE_GID})"
    groupadd -g ${ANSIBLE_GID} ${ANSIBLE_GROUP} 
+   if [ $? -ne 0 ]; then
+      (>&2 echo "${0}: ERROR. Creation of (${ANSIBLE_GROUP}) failed") 
+      exit 1
+   fi
 fi
 
 # check if the user ${ANSIBLE_USER} exist
 if ! check_if_user_exist ${ANSIBLE_USER} ; then
    # user ${ANSIBLE_USER} does NOT exist - creating
+   [ ${NOISE} ] || echo "${0}: Creating ${ANSIBLE_USER} user with UID (${ANSIBLE_UID})"
    create_user ${ANSIBLE_USER} ${ANSIBLE_GROUP} ${ANSIBLE_UID}
+   if [ $? -ne 0 ]; then
+      (>&2 echo "${0}: ERROR. Creation of (${ANSIBLE_USER}) failed") 
+      exit 1
+   fi
 fi
 
 # allow ansible to sudo without a password
 
+[ ${NOISE} ] || echo "${0}: Creating /etc/sudoers.d/ansible"
 echo ansible ALL = NOPASSWD : ALL > /etc/sudoers.d/ansible
 
 # create or update authorized_keys
 
 if [ ! -d /home/${ANSIBLE_USER}/.ssh ]; then
    # there is no such directory - create it
+   [ ${NOISE} ] || echo "${0}: Creating /home/${ANSIBLE_USER}/.ssh"
    mkdir /home/${ANSIBLE_USER}/.ssh
    chown ${ANSIBLE_USER}:${ANSIBLE_GROUP} /home/${ANSIBLE_USER}/.ssh
    chmod 700 /home/${ANSIBLE_USER}/.ssh
+
+   # lets check if the directory was really created
+   if [ ! -d /home/${ANSIBLE_USER}/.ssh ]; then
+       (>&2 echo "${0}: ERROR. Creation of /home/${ANSIBLE_USER}/.ssh failed")
+       exit 1
+   fi     
 fi
 
 
 if [ ! -f /home/${ANSIBLE_USER}/.ssh/authorized_keys ]; then
    # there is no such file - create it
+   [ ${NOISE} ] || echo "${0}: Creating /home/${ANSIBLE_USER}/.ssh/authorized_keys"
    touch /home/${ANSIBLE_USER}/.ssh/authorized_keys 
    chown ${ANSIBLE_USER}:${ANSIBLE_GROUP} /home/${ANSIBLE_USER}/.ssh/authorized_keys
    chmod 600 /home/${ANSIBLE_USER}/.ssh/authorized_keys
+
+   #lets check if the file was created
+   if [ ! -f /home/${ANSIBLE_USER}/.ssh/authorized_keys ]; then
+       (>&2 echo "${0}: ERROR. Creation of /home/${ANSIBLE_USER}/.ssh/authorized_keys failed")
+       exit 1
+   fi
 fi
 
 # check if $AUTH_USER is already listed in authorized_keys
 grep -q -E "${AUTH_USER}" /home/${ANSIBLE_USER}/.ssh/authorized_keys > /dev/null
 if [ $? -ne 0 ] ;then
    # there is no such string in authorized_keys - add it!
+   [ ${NOISE} ] || echo "${0}: Adding key for ${AUTH_USER}"
    echo ${AUTH_KEY}${AUTH_USER} >> /home/${ANSIBLE_USER}/.ssh/authorized_keys
 fi
 
